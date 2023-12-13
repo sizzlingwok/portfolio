0a3cda122453821810b35a76b260d2886ea48a84
andy duong <andyduong510@gmail.com>
2023-12-12 22:51:54 -0800

    rename

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Routes, Route, BrowserRouter as Router } from 'react-router-dom';
 import Header from './components/Header';
 import Home from './pages/Home.jsx';
-import Spotify from './pages/Spotify.jsx';
+import Spotify from './pages/spotify.jsx';
 import Sorette from './pages/sorette.jsx';
 import Mechmarket from './pages/mechmarket.jsx';
 import ScrollToTop from './ScrollToTop';
