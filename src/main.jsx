f06ded61d1d04c411792e6dcac65da23d79f0bf9
andy duong <andyduong510@gmail.com>
2023-12-19 17:43:35 -0800

    vercel error fix

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
-
+import { HelmetProvider } from 'react-helmet-async';
 
 createRoot(document.getElementById("root")).render(
+  <HelmetProvider>
     <App />
+  </HelmetProvider>
 );
